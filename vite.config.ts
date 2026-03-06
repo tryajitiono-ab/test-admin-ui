25bded14f6638abdf0f4886388662f1b42ba06f9
Try Ajitiono <try.ajitiono@accelbyte.net>
2026-03-06 09:31:19 +0700

    yep

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,8 @@ export default defineConfig(({ mode }) => {
           '.': './src/mf-entry.ts'
         },
         shared: {
+          react: {},
+          'react-dom': {},
           '@tanstack/react-query': {}
         }
       })
